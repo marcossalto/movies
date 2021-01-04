1f65a095dba3e9ab756ec735f1a1532626d5d3b4
Marcos Salto <marcosasalto@gmail.com>
2021-01-04 09:14:10 -0300

    Create Main Activity

---
+++ app/src/main/java/com/marcossalto/movies/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.marcossalto.movies
+
+/**
+ * Created by Marcos Salto on 04/01/2021.
+ */
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
