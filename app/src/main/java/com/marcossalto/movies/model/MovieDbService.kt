df63d3cd85921ea1c857e9e63a349dc33bf49ca8
Marcos Salto <marcosasalto@gmail.com>
2021-01-04 13:23:02 -0300

    Find region and use it in the request

--- app/src/main/java/com/marcossalto/movies/model/MovieDbService.kt
+++ app/src/main/java/com/marcossalto/movies/model/MovieDbService.kt
@@ -9,5 +9,8 @@ import retrofit2.http.Query
 
 interface MovieDbService {
     @GET("discover/movie?sort_by=popularity.desc")
-    suspend fun listPopularMoviesAsync(@Query("api_key") apiKey: String): MovieDbResult
+    suspend fun listPopularMoviesAsync(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): MovieDbResult
 }
